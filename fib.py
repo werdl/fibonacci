2c3967c72f86a94182e9271131902cf614a90106
werdl <werdl_is_cool@outlook.com>
2023-10-19 17:49:01 +0100

    lol

--- fib.py
+++ fib.py
@@ -14,7 +14,7 @@ When I say double precision, more info can be found at IEEE 754-1985.
 This fixes most of those issues! Wahey!
 """
 
-from mpmath import power as pow, mpf, sqrt, mpi, log10
+from mpmath import power as pow, mpf, sqrt, mpi
 import mpmath
 import time
 import sys
@@ -35,26 +35,21 @@ def OneOrZero(prompt, fail):
         print(fail)
         OneOrZero(prompt,fail)
 class Fibonacci:
-    def __init__(self):
-        self.table=[]
-        mpmath.mp.prec=100
-        mpmath.mp.dps=100
 
-    @classmethod
-    def gen_table(self, n: int) -> list:
+    @staticmethod
+    def gen_table(n: int) -> list:
         """
         Generate a table of values for Fibonacci numbers up to n
         """
-        if "table" not in dir(self): # allows for calling on Fibonacci class or object
-            self.table=[]
-        self.table.append(0)
-        self.table.append(1)
-        for _ in range(2,n):
-            self.table.append(self.table[-1]+self.table[-2])
-        return self.table
+        table=[]
+        table.append(0)
+        table.append(1)
+        for _ in range(2,n): # discard counter
+            table.append(table[-1]+table[-2])
+        return table
    
-    @classmethod
-    def nth(self, n: int, accuracy: int = 1000) -> int:
+    @staticmethod
+    def nth(n: int, accuracy: int = 1000) -> int:
         if accuracy>650:
             sys.set_int_max_str_digits(accuracy+10)
         mpmath.mp.prec=accuracy # we set these to the same value, but they
@@ -78,16 +73,22 @@ class Fibonacci:
         mpmath.nprint(value, len(splitted[0])) # whole number
         return (value,end-start)
    
-    @classmethod
-    def is_fib(self, n: int):
-        if float(5*n+4).is_integer():
+    @staticmethod
+    def is_fib(n: int):
+        """
+        Is n in the Fibonacci sequence?
+        It is actually trivial to find out - if 5n+4 or 5n-4 is a perfect square
+        """
+        if float(sqrt(5*n+4)).is_integer():
             return True
-        elif float(5*n-4).is_integer():
+        elif float(sqrt(5*n-4)).is_integer():
             return True
         else:
             return False
 
-max_n=int(input("Please enter the term number of the number you want: "))
+max_n=abs(int(input("Please enter the term number of the number \
+you want (negatives will be abs'ed): ")))
+all_done=0
 if not OneOrZero("Would you like a list (0) or the term (1)? ",
                  "Please enter a value that is either 0 or 1."):
     start_table=time.time()
@@ -95,10 +96,10 @@ if not OneOrZero("Would you like a list (0) or the term (1)? ",
     end_table=time.time()
     print(str(result).replace("'","").replace("[","").replace("]",""))
     print(f"That that took {end_table-start_table}s")
+
 else:
     start_raw=time.time()
-    result=Fibonacci.nth(max_n, max_n)
+    result=Fibonacci.nth(max_n, max_n) # why not max_n/4? python rounding :)
     end_raw=time.time()
     print(f"That that took {result[1]}s to do the actual maths, and \
 {end_raw-start_raw}s overall")
-
